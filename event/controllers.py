a55d08b29a6931cc416d77f373b35e8c40d44911
cikay <muzaffer123cikay@gmail.com>
2022-03-10 15:54:29 +0300

    Add creating event endpoint

---
+++ event/controllers.py
@@ -0,0 +1,29 @@
+
+from asyncio import events
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+
+from auth.models import UserModel
+from auth.schemas import UserCreate
+from auth.utils import get_current_user
+from event.models import EventModel
+from event.schemas import EventCreate
+from db_setup import get_db
+
+event_router = APIRouter(prefix='/events')
+
+@event_router.post('/create')
+def create_event(
+    event_schema: EventCreate,
+    db: Session=Depends(get_db),
+    current_user: UserCreate=Depends(get_current_user)
+):
+    event_model = EventModel(
+        **event_schema.dict(),
+        created_by=current_user['user_id']
+    )
+    db.add(event_model)
+    db.commit()
+    db.refresh(event_model)
+    return event_model
---
+++ event/schemas.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from pydoc import describe
+from datetime import datetime
+
+from pydantic import BaseModel
+
+class EventCreate(BaseModel):
+    description: str
+    total_tickets_count: int
+    open_window: datetime
