472ef8cc2470c2e5ab3a7f1e5eab4571e718d240
cikay <muzaffer123cikay@gmail.com>
2022-03-10 12:43:59 +0300

    Add creating user test

---
+++ tests/test_user.py
@@ -0,0 +1,34 @@
+import random
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def generate_random_string(length):
+    sample_string = 'abcdefghijklmnopqrstuvwxy_123456789'
+    return ''.join((random.choice(sample_string)) for _ in range(length))  
+
+
+def test_create_user():
+    random_string = generate_random_string(10)
+    fields = {
+        'firstname': f'Test {random_string}',
+        'lastname': f'Test {random_string}',
+        'username': f'test_{random_string}',
+        'password': f'test_{random_string}',
+        'is_admin': False
+    }
+    response = client.post('/users/create', json=fields)
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['username'] == fields['username']
+    assert data['is_admin'] is False
+    assert 'id' in data
+
+
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['username'] == common_fields['username']
+    assert data['is_admin'] is False
+    assert 'id' in data
