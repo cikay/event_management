552f36afb16b6e025731124c0befda4a374d8695
cikay <muzaffer123cikay@gmail.com>
2022-03-11 16:58:35 +0300

    Add listing tickets test

--- tests/test_event.py
+++ tests/test_event.py
@@ -424,3 +424,69 @@ def test_no_ticket_available():
         headers=headers
     )
     assert ticket_response.status_code == status.HTTP_403_FORBIDDEN
+
+
+def test_listing_tickets():
+    # create user
+    user_string = generate_random_string(10)
+    username = f'test_{user_string}'
+    password = f'test_{user_string}'
+
+    user_create_fields = {
+        'firstname': f'Test {user_string}',
+        'lastname': f'Test {user_string}',
+        'username': username,
+        'password': password,
+        'is_admin': True
+    }
+    response = client.post('/users/create', json=user_create_fields)
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['username'] == user_create_fields['username']
+    assert data['is_admin'] is True
+    assert 'id' in data
+
+    #login user
+    login_fields = {
+        'username': username,
+        'password': password
+    }
+    response = client.post('/users/login', data=login_fields)
+    token_data = response.json()
+    assert 'access_token' in token_data
+
+    # create event
+    event_description = generate_random_string(10)
+    total_tickets_count = random.randint(20, 1000)
+    open_window = str(datetime.now() + timedelta(days=10))
+    event_create_fields = {
+        'description': f'Test Event {event_description}',
+        'total_tickets_count': total_tickets_count,
+        'open_window': open_window,
+        'start_date': str(datetime.now() + timedelta(days=20)),
+        'end_date': str(datetime.now() + timedelta(days=20, hours=2))
+    }
+    headers = {
+        'Authorization': f'{token_data["token_type"]} {token_data["access_token"]}'
+    }
+    response = client.post('/events/create', json=event_create_fields, headers=headers)
+    assert response.status_code == status.HTTP_200_OK
+    created_event_data = response.json()
+    assert created_event_data['total_tickets_count'] == created_event_data['remain_tickets_count']
+
+    ticket_create_fields = {
+        'event_id': created_event_data['id'],
+        'seat_no': 'A1'
+    }
+
+    ticket_response = client.post(
+        '/tickets/create',
+        json=ticket_create_fields,
+        headers=headers
+    )
+    assert ticket_response.status_code == status.HTTP_200_OK
+
+    ticket_list_response = client.get('tickets/', headers=headers)
+    ticket_list_response.status_code == status.HTTP_200_OK
+    ticket_list = ticket_list_response.json()
+    assert 'id' in ticket_list[0]
