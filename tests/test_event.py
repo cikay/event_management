9fc0981b7ef419732ddefa1acedaf83b1262f76a
cikay <muzaffer123cikay@gmail.com>
2022-03-11 11:23:03 +0300

    Add user admin partial update test

--- tests/test_event.py
+++ tests/test_event.py
@@ -104,3 +104,68 @@ def test_admin_user_create_event():
     response = client.post('/events/create', json=event_create_fields, headers=headers)
     assert response.status_code == status.HTTP_200_OK
 
+
+def test_admin_user_update_event():
+    #create user
+    user_string = generate_random_string(10)
+    username = f'test_{user_string}'
+    password = f'test_{user_string}'
+
+    user_create_fields = {
+        'firstname': f'Test {user_string}',
+        'lastname': f'Test {user_string}',
+        'username': username,
+        'password': password,
+        'is_admin': True
+    }
+    response = client.post('/users/create', json=user_create_fields)
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data['username'] == user_create_fields['username']
+    assert data['is_admin'] is True
+    assert 'id' in data
+
+
+    #login user
+    login_fields = {
+        'username': username,
+        'password': password
+    }
+    response = client.post('/users/login', data=login_fields)
+    data = response.json()
+    assert 'access_token' in data
+
+
+    # create event
+    total_tickets_count = random.randint(20, 1000)
+    event_description = generate_random_string(10)
+    open_window = str(datetime.now() + timedelta(days=10))
+    event_create_fields = {
+        'description': f'Test Event {event_description}',
+        'total_tickets_count': total_tickets_count,
+        'open_window': open_window,
+        'start_date': str(datetime.now() + timedelta(days=20)),
+        'end_date': str(datetime.now() + timedelta(days=20, hours=2))
+    }
+    headers = {
+        'Authorization': f'Bearer {data["access_token"]}'
+    }
+    response = client.post('/events/create', json=event_create_fields, headers=headers)
+    assert response.status_code == status.HTTP_200_OK
+    created_event = response.json()
+    created_event_id = created_event['id']
+
+    # update event
+    event_new_description = generate_random_string(10)
+    update_open_window = str(datetime.now() + timedelta(days=20))
+    event_create_fields = {
+        'description': f'Test Event {event_new_description}',
+        'open_window': update_open_window
+    }
+
+    response = client.patch(
+        f'/events/update/{created_event_id}',
+        json=event_create_fields,
+        headers=headers
+    )
+    assert response.status_code == status.HTTP_200_OK
